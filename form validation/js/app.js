b9fdc801bf7cd4c079cb03ce4fc4144d0051db7c
annhhnguyen1711 <143236128+annhhnguyen1711@users.noreply.github.com>
2023-10-15 22:17:57 +0700

    btc

--- form validation/js/app.js
+++ form validation/js/app.js
@@ -51,5 +51,51 @@ const checkPassword=() =>{
     }else if(!isPasswordSecure(password)){
         showError(passwordEl, 'Password must has at least 8 charectes that include at least 1 lowercase'+
         'character, q uppercase characters, 1 number, and 1 speciad char')
+    }else{
+        showSuccess(passwordEl);
+        vaild=true;
     }
-}
+    return vaild;
+};
+
+
+const checkConfirmPassword = () => {
+    let vaild = false;
+    //check confirm password
+    const confirmPassword=confirmPasswordEl.value.trim();
+    const password=passwordEl.value.trim();
+
+    if(!isRequired(confirmPassword)){
+        showError(confirmPasswordEl, 'Please enter the password again ');
+    }else if(password !== confirmPassword){
+        showError(confirmPasswordEl, 'The password does not match');
+    }else{
+        showSuccess(confirmPasswordEl);
+        vaild = true;
+    }
+
+    return vaild;
+};
+
+const isEmailVaild = (email) =>{
+    //Regular expression (check email)
+ //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+ const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+ return re.test(email);
+};
+
+const isEmailValid = (email) => {
+    //Regular expression (check email)
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+};
+
+const isPasswordSecure = (password) => {
+    //Regular expression (check password)
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z]Ư(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return re.test(password);
+};
+
+const isRequired = value => value === ''? false : true;
+
